677debe932ee945eefae17da4a0d6a1a9d0fe295
Deepanshu kumar <114489502+Deepanshu0703@users.noreply.github.com>
2024-04-07 01:00:08 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -31,7 +31,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://www.linkedin.com/in/deepanshu-kumar-403200254/"
+                href="https://www.linkedin.com/in/deepanshu-kumar-/"
                 style={{ color: "white" }}
                 target="_blank"
                 rel="noopener noreferrer"
